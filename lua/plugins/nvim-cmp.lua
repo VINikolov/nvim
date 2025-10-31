065997886ff043e160ec4c36499a65ab7ae759c9
VINikolov <v.nikolov.95@gmail.com>
2025-10-31 11:51:09 +0200

    refactor: minor changes

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,49 +1,47 @@
 return {
-  {
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
+  'hrsh7th/nvim-cmp',
+  event = 'InsertEnter',
 
-    dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'onsails/lspkind.nvim',
-    },
-
-    opts = function()
-      local cmp = require 'cmp'
-      local lspkind = require 'lspkind'
-
-      return {
-        completion = {
-          completeopt = 'menu,menuone,noinsert,noselect',
-        },
-
-        mapping = cmp.mapping.preset.insert {
-          ['<C-S-Space>'] = cmp.mapping.complete(),
-          ['<CR>'] = cmp.mapping.confirm { select = true },
-        },
-
-        sources = cmp.config.sources {
-          {
-            name = 'nvim_lsp',
-
-            entry_filter = function(entry, _)
-              return require('cmp').lsp.CompletionItemKind.Snippet ~= entry:get_kind()
-            end,
-          },
-        },
-
-        formatting = {
-          format = lspkind.cmp_format {
-            mode = 'symbol_text',
-            maxwidth = 50,
-            ellipsis_char = '...',
-          },
-        },
-      }
-    end,
-
-    config = function(_, opts)
-      require('cmp').setup(opts)
-    end,
+  dependencies = {
+    'hrsh7th/cmp-nvim-lsp',
+    'onsails/lspkind.nvim',
   },
+
+  opts = function()
+    local cmp = require 'cmp'
+    local lspkind = require 'lspkind'
+
+    return {
+      completion = {
+        completeopt = 'menu,menuone,noinsert,noselect',
+      },
+
+      mapping = cmp.mapping.preset.insert {
+        ['<C-S-Space>'] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm { select = true },
+      },
+
+      sources = cmp.config.sources {
+        {
+          name = 'nvim_lsp',
+
+          entry_filter = function(entry, _)
+            return require('cmp').lsp.CompletionItemKind.Snippet ~= entry:get_kind()
+          end,
+        },
+      },
+
+      formatting = {
+        format = lspkind.cmp_format {
+          mode = 'symbol_text',
+          maxwidth = 50,
+          ellipsis_char = '...',
+        },
+      },
+    }
+  end,
+
+  config = function(_, opts)
+    require('cmp').setup(opts)
+  end,
 }
