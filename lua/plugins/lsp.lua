43ad87edcc0e250fd02ad90dead3ba0866a105ae
VINikolov <v.nikolov.95@gmail.com>
2025-11-14 08:22:07 +0200

    chore: clean up

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,25 +1,25 @@
 return {
-  {
-    'williamboman/mason-lspconfig.nvim',
-    opts = {
-      ensure_installed = {
-        'lua_ls',
-      },
+  'williamboman/mason-lspconfig.nvim',
+
+  opts = {
+    ensure_installed = {
+      'lua_ls',
     },
-    dependencies = {
-      {
-        'williamboman/mason.nvim',
-        opts = {
-          ui = {
-            icons = {
-              package_installed = '✓',
-              package_pending = '➜',
-              package_uninstalled = '✗',
-            },
+  },
+  dependencies = {
+    {
+      'williamboman/mason.nvim',
+
+      opts = {
+        ui = {
+          icons = {
+            package_installed = '✓',
+            package_pending = '➜',
+            package_uninstalled = '✗',
           },
         },
       },
-      'neovim/nvim-lspconfig',
     },
+    'neovim/nvim-lspconfig',
   },
 }
