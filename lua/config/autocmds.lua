d36f3629a986630139d6866e211fb440a716359a
VINikolov <v.nikolov.95@gmail.com>
2026-02-09 14:18:48 +0200

    refactor: minor changes

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -16,7 +16,7 @@ vim.api.nvim_create_autocmd('CursorHold', {
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
-    vim.highlight.on_yank()
+    vim.hl.on_yank()
   end,
   group = highlight_group,
   pattern = '*',
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,28 +2,25 @@ return {
   'stevearc/conform.nvim',
   event = { 'BufReadPre', 'BufNewFile', 'BufWritePre' },
 
-  config = function()
-    local conform = require 'conform'
+  opts = {
+    notify_on_error = true,
+    formatters_by_ft = {
+      javascript = { 'prettierd' },
+      typescript = { 'prettierd' },
+      javascriptreact = { 'prettierd' },
+      typescriptreact = { 'prettierd' },
+      css = { 'prettierd' },
+      html = { 'prettierd' },
+      json = { 'prettierd' },
+      lua = { 'stylua' },
+      python = { 'black' },
+      cs = { 'csharpier' },
+    },
 
-    conform.setup {
-      notify_on_error = true,
-      formatters_by_ft = {
-        javascript = { 'prettierd' },
-        typescript = { 'prettierd' },
-        javascriptreact = { 'prettierd' },
-        typescriptreact = { 'prettierd' },
-        css = { 'prettierd' },
-        html = { 'prettierd' },
-        json = { 'prettierd' },
-        lua = { 'stylua' },
-        python = { 'black' },
-        cs = { 'csharpier' },
-      },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      },
-    }
-  end,
+    format_on_save = {
+      lsp_fallback = true,
+      async = false,
+      timeout_ms = 1000,
+    },
+  },
 }
